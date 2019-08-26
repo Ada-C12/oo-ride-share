ef94786d5168a4a4c88aa677428233b12ca815f6
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-26 16:06:46 -0700

    Total_expenditure instance method for Passenger. Wrote multiple tests to check for correct calculation and data type. Modified method to make tests pass.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,21 +3,32 @@ require_relative 'csv_record'
 module RideShare
   class Passenger < CsvRecord
     attr_reader :name, :phone_number, :trips
-
+    
     def initialize(id:, name:, phone_number:, trips: nil)
       super(id)
-
+      
       @name = name
       @phone_number = phone_number
       @trips = trips || []
     end
-
+    
     def add_trip(trip)
       @trips << trip
     end
-
+    
+    def net_expenditures
+      total_expenditure = 0
+      trips.each do |trip|
+        total_expenditure += trip.cost
+      end
+      return total_expenditure
+      # loop through all the individual trips a given passenger has taken
+      # increment or add all costs to a local variable called total_expenditure 
+      # return total_expenditure 
+    end 
+    
     private
-
+    
     def self.from_csv(record)
       return new(
         id: record[:id],
--- test/passenger_test.rb
+++ test/passenger_test.rb
@@ -1,40 +1,86 @@
 require_relative 'test_helper'
 
 describe "Passenger class" do
-
+  
   describe "Passenger instantiation" do
     before do
       @passenger = RideShare::Passenger.new(id: 1, name: "Smithy", phone_number: "353-533-5334")
     end
-
+    
     it "is an instance of Passenger" do
       expect(@passenger).must_be_kind_of RideShare::Passenger
     end
-
+    
     it "throws an argument error with a bad ID value" do
       expect do
         RideShare::Passenger.new(id: 0, name: "Smithy")
       end.must_raise ArgumentError
     end
-
+    
     it "sets trips to an empty array if not provided" do
       expect(@passenger.trips).must_be_kind_of Array
       expect(@passenger.trips.length).must_equal 0
     end
-
+    
     it "is set up for specific attributes and data types" do
       [:id, :name, :phone_number, :trips].each do |prop|
         expect(@passenger).must_respond_to prop
       end
-
+      
       expect(@passenger.id).must_be_kind_of Integer
       expect(@passenger.name).must_be_kind_of String
       expect(@passenger.phone_number).must_be_kind_of String
       expect(@passenger.trips).must_be_kind_of Array
     end
   end
-
-
+  
+  describe "Net Expenditure" do
+    before do
+      @passenger = RideShare::Passenger.new(id: 1, name: "Lionel", phone_number: "353-533-7678")
+      
+      # trip 1
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_1_data = {
+        id: 8,
+        passenger: @passenger,
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.00,
+        rating: 3
+      }
+      @trip_1 = RideShare::Trip.new(@trip_1_data)
+      @passenger.add_trip(@trip_1)
+      
+      # trip 2
+      start_time_2 = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time_2 = start_time_2 + 30 * 60 # 25 minutes
+      @trip_2_data = {
+        id: 8,
+        passenger: @passenger,
+        start_time: start_time_2,
+        end_time: end_time_2,
+        cost: 50.00,
+        rating: 3
+      }
+      @trip_2 = RideShare::Trip.new(@trip_2_data)
+      @passenger.add_trip(@trip_2)
+    end
+    
+    
+    it "Total Expenditure is a Float" do
+      expect(@passenger.net_expenditures).must_be_instance_of Float 
+    end
+    
+    it "Is greater than 0" do
+      expect(@passenger.net_expenditures).must_be :>=, 0
+    end
+    
+    it "Is correctly caclulating the total expenditure" do
+      expect(@passenger.net_expenditures).must_equal 73.00
+    end 
+  end
+  
   describe "trips property" do
     before do
       # TODO: you'll need to add a driver at some point here.
@@ -43,32 +89,34 @@ describe "Passenger class" do
         name: "Merl Glover III",
         phone_number: "1-602-620-2330 x3723",
         trips: []
-        )
+      )
       trip = RideShare::Trip.new(
         id: 8,
         passenger: @passenger,
         start_time: "2016-08-08",
         end_time: "2016-08-09",
         rating: 5
-        )
-
+      )
+      
       @passenger.add_trip(trip)
     end
-
+    
     it "each item in array is a Trip instance" do
       @passenger.trips.each do |trip|
         expect(trip).must_be_kind_of RideShare::Trip
       end
     end
-
+    
     it "all Trips must have the same passenger's passenger id" do
       @passenger.trips.each do |trip|
         expect(trip.passenger.id).must_equal 9
       end
     end
-  end
-
-  describe "net_expenditures" do
-    # You add tests for the net_expenditures method
+    
+    
+    describe "net_expenditures" do
+      # You add tests for the net_expenditures method
+    end
+    
   end
 end
