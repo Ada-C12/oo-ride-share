7f824525969be3c60764a6ceacd14fece58195ac
Mira <calopter@gmail.com>
2019-08-26 15:50:03 -0700

    test that end time is after start time

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -18,6 +18,26 @@ describe "Trip class" do
       @trip = RideShare::Trip.new(@trip_data)
     end
 
+    it "raises ArgumentError if end_time is before start_time" do
+      #raise ArgumentError if end_time < start_time
+      
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + 25 * 60 # 25 minutes
+      
+      trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+                                            name: "Ada",
+                                            phone_number: "412-432-7640"),
+        end_time: start_time,
+        start_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      
+      expect { RideShare::Trip.new(trip_data) }.must_raise ArgumentError
+    end
+
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
     end
