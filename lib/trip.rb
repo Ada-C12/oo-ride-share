65f58760d2efaa5fef727cb1ac0ba6d6e17d504d
Sabrina Lowney <sabrina@galaxylaughing.com>
2019-08-26 15:27:14 -0700

    modified trip start and end times to be Time instances, created a duration method for trip, created tests for the duration method, changed spec/test_data to test/test_data

--- lib/trip.rb
+++ lib/trip.rb
@@ -1,37 +1,38 @@
 require 'csv'
+require 'time'
 
 require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
     attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
-
+    
     def initialize(id:,
       passenger: nil, passenger_id: nil,
       start_time:, end_time:, cost: nil, rating:)
       super(id)
-
+      
       if passenger
         @passenger = passenger
         @passenger_id = passenger.id
-
+        
       elsif passenger_id
         @passenger_id = passenger_id
-
+        
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
       end
-
+      
       @start_time = start_time
       @end_time = end_time
       @cost = cost
       @rating = rating
-
+      
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
     end
-
+    
     def inspect
       # Prevent infinite loop when puts-ing a Trip
       # trip contains a passenger contains a trip contains a passenger...
@@ -39,15 +40,38 @@ module RideShare
       "ID=#{id.inspect} " +
       "PassengerID=#{passenger&.id.inspect}>"
     end
-
+    
     def connect(passenger)
       @passenger = passenger
       passenger.add_trip(self)
     end
-
+    
+    # duration method
+    # instance method, difference between start time and end time
+    # def duration
+    #   difference = end - start
+    #   IF difference is less than or equal to zero
+    #     raise argument error
+    #   END
+    # end
+    def duration
+      difference = end_time - start_time
+      if difference == 0
+        raise ArgumentError.new("Duration cannot be zero")
+      end
+      return difference
+    end
+    
     private
     
     def self.from_csv(record)
+      # Modify Trip.from_csv to turn start_time and end_time into Time instances before passing them to Trip#initialize
+      
+      # START => 2018-12-27 02:39:05 -0800
+      # END => 2018-12-27 03:38:08 -0800
+      start_time = Time.parse(record[:start_time])
+      end_time = Time.parse(record[:end_time])
+      
       return self.new(
         id: record[:id],
         passenger_id: record[:passenger_id],
@@ -55,7 +79,9 @@ module RideShare
         end_time: record[:end_time],
         cost: record[:cost],
         rating: record[:rating]
-        )
+      )
     end
   end
 end
+
+# duration = end_time - start_time
--- test/trip_test.rb
+++ test/trip_test.rb
@@ -7,9 +7,7 @@ describe "Trip class" do
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
         id: 8,
-        passenger: RideShare::Passenger.new(id: 1,
-                                            name: "Ada",
-                                            phone_number: "412-432-7640"),
+        passenger: RideShare::Passenger.new(id: 1, name: "Ada", phone_number: "412-432-7640"),
         start_time: start_time,
         end_time: end_time,
         cost: 23.45,
@@ -17,20 +15,20 @@ describe "Trip class" do
       }
       @trip = RideShare::Trip.new(@trip_data)
     end
-
+    
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
     end
-
+    
     it "stores an instance of passenger" do
       expect(@trip.passenger).must_be_kind_of RideShare::Passenger
     end
-
+    
     it "stores an instance of driver" do
       skip # Unskip after wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
     end
-
+    
     it "raises an error for an invalid rating" do
       [-3, 0, 6].each do |rating|
         @trip_data[:rating] = rating
@@ -39,5 +37,55 @@ describe "Trip class" do
         end.must_raise ArgumentError
       end
     end
+    
   end
+  
+  describe "duration" do
+    
+    it "must be a positive number" do
+      # Arrange
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1, name: "Ada", phone_number: "412-432-7640"),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      @trip = RideShare::Trip.new(@trip_data)
+      # Act
+      difference_in_time = @trip.duration
+      # Assert
+      expect(difference_in_time).must_be :>, 0
+    end
+    
+    it "must raise error if zero" do
+      # Arrange
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = Time.parse('2015-05-20T12:14:00+00:00')
+      @new_trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1, name: "Ada", phone_number: "412-432-7640"),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+      @new_trip = RideShare::Trip.new(@new_trip_data)
+      # Act && Assert
+      expect do
+        @new_trip.duration
+      end.must_raise ArgumentError
+    end
+    
+    it "must raise error if negative number" do
+    end
+    
+    it "must raise error if nil" do
+    end
+    
+  end
+  
 end
