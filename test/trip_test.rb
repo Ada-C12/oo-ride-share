4a23f8df1a14a7bae9a0d3547988ffa3e7eca1c6
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-27 14:53:29 -0700

    created unit tests for 'duration' method and time validation in 'initialize' method

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -1,4 +1,5 @@
 require_relative 'test_helper'
+require 'time'
 
 describe "Trip class" do
   describe "initialize" do
@@ -8,8 +9,9 @@ describe "Trip class" do
       @trip_data = {
         id: 8,
         passenger: RideShare::Passenger.new(id: 1,
-                                            name: "Ada",
-                                            phone_number: "412-432-7640"),
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
         start_time: start_time,
         end_time: end_time,
         cost: 23.45,
@@ -17,20 +19,20 @@ describe "Trip class" do
       }
       @trip = RideShare::Trip.new(@trip_data)
     end
-
+    
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
     end
-
+    
     it "stores an instance of passenger" do
       expect(@trip.passenger).must_be_kind_of RideShare::Passenger
     end
-
+    
     it "stores an instance of driver" do
       skip # Unskip after wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
     end
-
+    
     it "raises an error for an invalid rating" do
       [-3, 0, 6].each do |rating|
         @trip_data[:rating] = rating
@@ -38,6 +40,44 @@ describe "Trip class" do
           RideShare::Trip.new(@trip_data)
         end.must_raise ArgumentError
       end
+    end
+    
+    it "raises an ArgumentError if end time is before start time" do
+      @trip_data[:end_time] = @trip_data[:start_time] - 25 * 60
+      expect do
+        RideShare::Trip.new(@trip_data)
+      end.must_raise ArgumentError
+    end
+  end
+  
+  describe "duration" do
+    before do
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_data = {
+        id: 8,
+        passenger: RideShare::Passenger.new(id: 1,
+          name: "Ada",
+          phone_number: "412-432-7640"
+        ),
+        start_time: start_time,
+        end_time: end_time,
+        cost: 23.45,
+        rating: 3
+      }
+    end
+    
+    it "calculates the total trip time" do
+      trip = RideShare::Trip.new(@trip_data)
+      result = trip.duration
+      expect(result).must_equal 1500
+    end
+    
+    it "returns 0 if start and end time were the same" do
+      @trip_data[:end_time] = @trip_data[:start_time]
+      trip = RideShare::Trip.new(@trip_data)
+      result = trip.duration
+      expect(result).must_equal 0
     end
   end
 end
