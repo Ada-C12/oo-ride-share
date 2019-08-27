6f30069fd073038e8d587e1ec4324b73c4985bcf
Eve Le <nguyenthao.lee@gmail.com>
2019-08-27 14:57:00 -0700

    Added tests and implementation for net_expenditures for Passenger class

--- test/passenger_test.rb
+++ test/passenger_test.rb
@@ -1,74 +1,116 @@
 require_relative 'test_helper'
 
 describe "Passenger class" do
-
+  
   describe "Passenger instantiation" do
     before do
       @passenger = RideShare::Passenger.new(id: 1, name: "Smithy", phone_number: "353-533-5334")
     end
-
+    
     it "is an instance of Passenger" do
       expect(@passenger).must_be_kind_of RideShare::Passenger
     end
-
+    
     it "throws an argument error with a bad ID value" do
       expect do
         RideShare::Passenger.new(id: 0, name: "Smithy", phone_number: "353-223-3452")
       end.must_raise ArgumentError
     end
-
+    
     it "sets trips to an empty array if not provided" do
       expect(@passenger.trips).must_be_kind_of Array
       expect(@passenger.trips.length).must_equal 0
     end
-
+    
     it "is set up for specific attributes and data types" do
       [:id, :name, :phone_number, :trips].each do |prop|
         expect(@passenger).must_respond_to prop
       end
-
+      
       expect(@passenger.id).must_be_kind_of Integer
       expect(@passenger.name).must_be_kind_of String
       expect(@passenger.phone_number).must_be_kind_of String
       expect(@passenger.trips).must_be_kind_of Array
     end
   end
-
-
+  
+  
   describe "trips property" do
     before do
       # TODO: you'll need to add a driver at some point here.
       @passenger = RideShare::Passenger.new(
-        id: 9,
-        name: "Merl Glover III",
-        phone_number: "1-602-620-2330 x3723",
-        trips: []
-        )
+      id: 9,
+      name: "Merl Glover III",
+      phone_number: "1-602-620-2330 x3723",
+      trips: []
+      )
       trip = RideShare::Trip.new(
-        id: 8,
-        passenger: @passenger,
-        start_time: "2016-08-08",
-        end_time: "2016-08-09",
-        rating: 5
-        )
-
+      id: 8,
+      passenger: @passenger,
+      start_time: Time.parse("2019-01-09 08:30:31 -0800"),
+      end_time: Time.parse("2019-01-09 08:48:50 -0800"),
+      rating: 5
+      )
+      
       @passenger.add_trip(trip)
     end
-
+    
     it "each item in array is a Trip instance" do
       @passenger.trips.each do |trip|
         expect(trip).must_be_kind_of RideShare::Trip
       end
     end
-
+    
     it "all Trips must have the same passenger's passenger id" do
       @passenger.trips.each do |trip|
         expect(trip.passenger.id).must_equal 9
       end
     end
   end
-
+  
   describe "net_expenditures" do
-    # You add tests for the net_expenditures method
+    before do
+      @passenger = RideShare::Passenger.new(
+      id: 9,
+      name: "Merl Glover III",
+      phone_number: "1-602-620-2330 x3723",
+      trips: [])
+      
+      trip1 = RideShare::Trip.new(
+      id: 8,
+      passenger: @passenger,
+      start_time: Time.parse("2019-01-09 08:30:31 -0800"),
+      end_time: Time.parse("2019-01-09 08:48:50 -0800"),
+      cost: 7,
+      rating: 5)
+      
+      trip2 = RideShare::Trip.new(
+      id: 10,
+      passenger: @passenger,
+      start_time: Time.parse("2019-01-17 04:33:18 -0800"),
+      end_time: Time.parse("2019-01-17 05:15:59 -0800"),
+      cost: 20,
+      rating: 2)
+      
+      @passenger.add_trip(trip1)
+      @passenger.add_trip(trip2)
+    end
+    
+    it "returns total amount of money passenger spent on their trips" do
+      expect (@passenger.net_expenditures).must_equal 27
+    end
+    
+    it "returns an number" do
+      expect (@passenger.net_expenditures).must_be_kind_of Numeric
+    end
+    
+    it "returns 0 if passenger doesn't have any trip" do
+      no_trip_passenger = RideShare::Passenger.new(
+      id: 9,
+      name: "Merl Glover III",
+      phone_number: "1-602-620-2330 x3723",
+      trips: [])
+      expect (no_trip_passenger.net_expenditures).must_equal 0
+    end
   end
 end
