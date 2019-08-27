d8ce4419825edadfc7ede2b00a9e544fff3b32eb
Kelsey Krippaehne <kelseykripp@gmail.com>
2019-08-26 17:04:01 -0700

    Initiated wave 1 methods: added time parsing to Trip, added 'calculate_duration' method to Trip, added 'net_expenditures' to Passenger, added tests for methods.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,28 +1,36 @@
 require_relative 'csv_record'
+require 'time'
 
 module RideShare
   class Passenger < CsvRecord
     attr_reader :name, :phone_number, :trips
-
+    
     def initialize(id:, name:, phone_number:, trips: nil)
       super(id)
-
+      
       @name = name
       @phone_number = phone_number
       @trips = trips || []
     end
-
+    
     def add_trip(trip)
       @trips << trip
     end
-
+    
+    def net_expenditures
+      total_trips = @trips.map do |trip|
+        trip.cost
+      end
+      total_trips.sum
+    end
+    
     private
-
+    
     def self.from_csv(record)
       return new(
-        id: record[:id],
-        name: record[:name],
-        phone_number: record[:phone_num]
+      id: record[:id],
+      name: record[:name],
+      phone_number: record[:phone_num]
       )
     end
   end
--- lib/trip.rb
+++ lib/trip.rb
@@ -1,37 +1,43 @@
 require 'csv'
+require 'time'
 
 require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
     attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
-
+    
     def initialize(id:,
       passenger: nil, passenger_id: nil,
       start_time:, end_time:, cost: nil, rating:)
       super(id)
-
+      
       if passenger
         @passenger = passenger
         @passenger_id = passenger.id
-
+        
       elsif passenger_id
         @passenger_id = passenger_id
-
+        
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
       end
-
+      
       @start_time = start_time
       @end_time = end_time
       @cost = cost
       @rating = rating
-
+      
       if @rating > 5 || @rating < 1
         raise ArgumentError.new("Invalid rating #{@rating}")
       end
-    end
 
+      if @end_time < @start_time
+        raise ArgumentError
+      end 
+
+    end
+    
     def inspect
       # Prevent infinite loop when puts-ing a Trip
       # trip contains a passenger contains a trip contains a passenger...
@@ -39,23 +45,37 @@ module RideShare
       "ID=#{id.inspect} " +
       "PassengerID=#{passenger&.id.inspect}>"
     end
-
+    
     def connect(passenger)
       @passenger = passenger
       passenger.add_trip(self)
     end
 
+    def calculate_duration 
+      start_time_seconds = @start_time.hour * 3600 + @start_time.min * 60 + @start_time.sec 
+
+      end_time_seconds = @end_time.hour * 3600 + @end_time.min * 60 + @end_time.sec 
+
+    end_time_seconds - start_time_seconds 
+
+    end 
     private
     
     def self.from_csv(record)
+      
+      parsed_start_time = Time.parse(record[:start_time])
+      parsed_end_time = Time.parse(record[:end_time])
+      
       return self.new(
-        id: record[:id],
-        passenger_id: record[:passenger_id],
-        start_time: record[:start_time],
-        end_time: record[:end_time],
-        cost: record[:cost],
-        rating: record[:rating]
-        )
+      id: record[:id],
+      passenger_id: record[:passenger_id],
+      start_time: parsed_start_time,
+      end_time: parsed_end_time,
+      cost: record[:cost],
+      rating: record[:rating]
+      )
     end
+  
+
   end
 end
