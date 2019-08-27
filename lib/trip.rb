fe101ac030b385e87976363934594a6ca6748de1
Paige Davenport <davenport.paige@outlook.com>
2019-08-27 15:28:43 -0700

    Updated from_csv method to included driver_id

--- lib/trip.rb
+++ lib/trip.rb
@@ -4,15 +4,15 @@ require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
-    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
+    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating, :driver_id, :driver
     
     def initialize(id:,
       passenger: nil, passenger_id: nil,
-      start_time:, end_time:, cost: nil, rating:)
+      start_time:, end_time:, cost: nil, rating:, driver_id: nil, driver: nil)
       super(id)
 
       raise ArgumentError if start_time > end_time
-      
+      #TODO: Refactor this!
       if passenger
         @passenger = passenger
         @passenger_id = passenger.id
@@ -22,6 +22,17 @@ module RideShare
         
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
+      end
+
+      if driver
+        @driver = driver
+        @driver_id = driver.id
+        
+      elsif driver_id
+        @driver_id = driver_id
+        
+      else
+        raise ArgumentError, 'Driver or driver_id is required'
       end
       
       @start_time = start_time
@@ -60,7 +71,8 @@ module RideShare
         start_time: Time.parse(record[:start_time]),
         end_time: Time.parse(record[:end_time]),
         cost: record[:cost],
-        rating: record[:rating]
+        rating: record[:rating],
+        driver_id: record[:driver_id]
       )
     end
   end
