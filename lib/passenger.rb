266fe9332799e8de98dc82ec23feddb0875c9bb7
Kristina <kristina.tanya@gmail.com>
2019-08-27 14:44:16 -0700

    Created net_expenditures test for passenger

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,26 +3,39 @@ require_relative 'csv_record'
 module RideShare
   class Passenger < CsvRecord
     attr_reader :name, :phone_number, :trips
-
+    
     def initialize(id:, name:, phone_number:, trips: nil)
       super(id)
-
+      
       @name = name
       @phone_number = phone_number
       @trips = trips || []
     end
-
+    
     def add_trip(trip)
       @trips << trip
     end
-
+    
+    def net_expenditures
+      # CHECK we know the number of trips this passenger has taken 
+      # CHECK we need to use Trips to get access to the cost of each trip
+      # CHECK we need to pair each trip with it's cost based off of a common value; like ID
+      # CHECK add all the trips into an array
+      # add all the costs into an array 
+      # sum all the trip costs an return the total spent
+      
+      
+      
+    end
+    
+    
     private
-
+    
     def self.from_csv(record)
       return new(
-        id: record[:id],
-        name: record[:name],
-        phone_number: record[:phone_num]
+      id: record[:id],
+      name: record[:name],
+      phone_number: record[:phone_num]
       )
     end
   end
--- test/passenger_test.rb
+++ test/passenger_test.rb
@@ -38,19 +38,8 @@ describe "Passenger class" do
   describe "trips property" do
     before do
       # TODO: you'll need to add a driver at some point here.
-      @passenger = RideShare::Passenger.new(
-      id: 9,
-      name: "Merl Glover III",
-      phone_number: "1-602-620-2330 x3723",
-      trips: []
-      )
-      trip = RideShare::Trip.new(
-      id: 8,
-      passenger: @passenger,
-      start_time: Time.parse("2016-08-08"),
-      end_time: Time.parse("2016-08-09"),
-      rating: 5
-      )
+      @passenger = RideShare::Passenger.new(id: 9, name: "Merl Glover III", phone_number: "1-602-620-2330 x3723", trips: [])
+      trip = RideShare::Trip.new(id: 8, passenger: @passenger, start_time: Time.parse("2016-08-08"), end_time: Time.parse("2016-08-09"), rating: 5)
       
       @passenger.add_trip(trip)
     end
@@ -61,14 +50,37 @@ describe "Passenger class" do
       end
     end
     
-    it "all Trips must have the same passenger's passenger id" do
-      @passenger.trips.each do |trip|
-        expect(trip.passenger.id).must_equal 9
-      end
-    end
+    # it "all Trips must have the same passenger's passenger id" do
+    #   @passenger.trips.each do |trip|
+    #     expect(trip.passenger.id).must_equal 9
+    #   end
+    # end
   end
   
   describe "net_expenditures" do
-    # You add tests for the net_expenditures method
+    it "Will return total amount a passenger spent on their trips taken" do
+      # Arrange
+      @passenger = RideShare::Passenger.new(id: 1, name: "Smithy", phone_number: "353-533-5334")
+      trip1 = RideShare::Trip.new(id: 8, passenger: @passenger, start_time: Time.parse("2016-08-08"), end_time: Time.parse("2016-08-09"), cost: 2, rating: 5)
+      trip2 = RideShare::Trip.new(id: 9, passenger: @passenger, start_time: Time.parse("2016-08-10"), end_time: Time.parse("2016-08-11"), cost: 10, rating: 4)
+      @passenger.add_trip(trip1)
+      @passenger.add_trip(trip2)
+      # p @passenger.trips.length
+      
+      trip_costs = []
+      trip_costs << @passenger.trips[0].cost
+      trip_costs << @passenger.trips[1].cost
+      
+      total_cost = trip_costs.sum
+      p total_cost
+      # Act
+      
+      # Assert
+      expect(total_cost).must_equal 12
+      
+    end
+    
+    
+    
   end
 end
