14333d55632476f200aec148323dd05e51563dca
dora1405 <li.dora05@gmail.com>
2019-08-27 16:21:47 -0700

    Wave 1.2, Step 1 complete in passenger.rb and passenger_test rb.

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,23 +1,33 @@
 require_relative 'csv_record'
 
+
 module RideShare
   class Passenger < CsvRecord
     attr_reader :name, :phone_number, :trips
-
+    
     def initialize(id:, name:, phone_number:, trips: nil)
       super(id)
-
+      
       @name = name
       @phone_number = phone_number
       @trips = trips || []
     end
-
+    
     def add_trip(trip)
       @trips << trip
     end
-
+    
+    def net_expenditures
+      costs = []
+      trips.each do |trip|
+        costs << trip.cost
+      end
+      return total_cost = costs.sum
+    end
+    
+    
     private
-
+    
     def self.from_csv(record)
       return new(
         id: record[:id],
@@ -25,5 +35,9 @@ module RideShare
         phone_number: record[:phone_num]
       )
     end
+    
+    # Add an instance method, net_expenditures, to Passenger that will return the total amount of money that passenger has spent on their trips
+    
+    
   end
 end
