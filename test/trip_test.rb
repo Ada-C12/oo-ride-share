9262923fc11ba2e5a6bb7d2dde4b2163504cd76f
brilatimer <brihiramine@gmail.com>
2019-08-26 18:10:41 -0700

    worked through wave 1 (1.1)

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -6,31 +6,32 @@ describe "Trip class" do
       start_time = Time.parse('2015-05-20T12:14:00+00:00')
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
-        id: 8,
-        passenger: RideShare::Passenger.new(id: 1,
-                                            name: "Ada",
-                                            phone_number: "412-432-7640"),
-        start_time: start_time,
-        end_time: end_time,
-        cost: 23.45,
-        rating: 3
-      }
+      id: 8,
+      passenger: RideShare::Passenger.new(
+      id: 1,
+      name: "Ada",
+      phone_number: "412-432-7640"
+      ),
+      start_time: start_time,
+      end_time: end_time,
+      cost: 23.45,
+      rating: 3}
       @trip = RideShare::Trip.new(@trip_data)
     end
-
+    
     it "is an instance of Trip" do
       expect(@trip).must_be_kind_of RideShare::Trip
     end
-
+    
     it "stores an instance of passenger" do
       expect(@trip.passenger).must_be_kind_of RideShare::Passenger
     end
-
+    
     it "stores an instance of driver" do
       skip # Unskip after wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
     end
-
+    
     it "raises an error for an invalid rating" do
       [-3, 0, 6].each do |rating|
         @trip_data[:rating] = rating
@@ -39,5 +40,36 @@ describe "Trip class" do
         end.must_raise ArgumentError
       end
     end
+    
+    it "raises an error for an end time that occurs before a start time" do
+      @trip_data[:end_time] -= 300 * 60 
+      expect do
+        RideShare::Trip.new(@trip_data)
+      end.must_raise ArgumentError
+    end
   end
-end
+  
+  describe "time duration" do
+    before do
+      start_time = Time.parse('2015-05-20T12:14:00+00:00')
+      end_time = start_time + 25 * 60 # 25 minutes
+      @trip_data = {
+      id: 8,
+      passenger: RideShare::Passenger.new(
+      id: 1,
+      name: "Ada",
+      phone_number: "412-432-7640"
+      ),
+      start_time: start_time,
+      end_time: end_time,
+      cost: 23.45,
+      rating: 3}
+      @trip = RideShare::Trip.new(@trip_data)
+    end
+    
+    it "returns trip duration in seconds" do 
+      expect(@trip.duration).must_equal 1500
+    end
+  end
+  
+end 
