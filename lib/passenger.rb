4e37861a30fd4cc13875cd6a8cc56f9cbd0747f5
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-27 15:52:10 -0700

    Defined methods 'net_expenditures' and 'total_time_spent' in passenger.rb

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -3,21 +3,37 @@ require_relative 'csv_record'
 module RideShare
   class Passenger < CsvRecord
     attr_reader :name, :phone_number, :trips
-
+    
     def initialize(id:, name:, phone_number:, trips: nil)
       super(id)
-
+      
       @name = name
       @phone_number = phone_number
       @trips = trips || []
     end
-
+    
     def add_trip(trip)
       @trips << trip
     end
-
+    
+    def net_expenditures
+      total_spending = 0
+      @trips.each do |trip|
+        total_spending += trip.cost
+      end
+      return total_spending
+    end
+    
+    def total_time_spent
+      total_time = 0
+      @trips.each do |trip|
+        total_time += trip.duration
+      end
+      return total_time
+    end
+    
     private
-
+    
     def self.from_csv(record)
       return new(
         id: record[:id],
