cfa6b32520ad94463fe11921174d1d9b8762f4a9
Ga-Young Jin <gyjinn@gmail.com>
2019-08-26 17:09:18 -0700

    Completed wave-1.2

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -1,23 +1,40 @@
 require_relative 'csv_record'
+require 'pry'
 
 module RideShare
   class Passenger < CsvRecord
     attr_reader :name, :phone_number, :trips
-
+    
     def initialize(id:, name:, phone_number:, trips: nil)
       super(id)
-
+      
       @name = name
       @phone_number = phone_number
       @trips = trips || []
     end
-
+    
     def add_trip(trip)
       @trips << trip
     end
-
+    
+    def net_expenditures
+      total_cost = 0
+      trips.each do |trip|
+        total_cost += trip.cost
+      end
+      return total_cost
+    end
+    
+    def total_time_spent
+      total_time = 0
+      trips.each do |trip|
+        total_time += trip.duration
+      end
+      return total_time
+    end
+    
     private
-
+    
     def self.from_csv(record)
       return new(
         id: record[:id],
@@ -27,3 +44,4 @@ module RideShare
     end
   end
 end
+
