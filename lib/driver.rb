6219a39963f8587a1a94db92410f7942e80f75d1
Eve Le <nguyenthao.lee@gmail.com>
2019-08-27 15:52:02 -0700

    Created Driver class and added constructor and path to file in test_helper.rb

---
+++ lib/driver.rb
@@ -0,0 +1,27 @@
+require_relative 'csv_record'
+
+module RideShare
+  class Driver < CsvRecord
+    STATUSES = [:AVAILABLE, :UNAVAILABLE]
+    
+    attr_reader :name, :vin, :status, :trips
+    
+    def initialize(id:, name:, vin:, status: :AVAILABLE, trips: [])
+      super(id)
+      
+      @name = name
+      @vin = vin
+      @status = status
+      @trips = trips
+      
+      if @vin.length != 17
+        raise ArgumentError.new("VIN must contain 17 characters")
+      end
+      
+      if !STATUSES.include? @status
+        raise ArgumentError.new("Invalid status")
+      end
+      
+    end  
+  end
+end
