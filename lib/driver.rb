5b3131e163c0123ceb16bbc3bb498c007b6ed100
Diana Han <dianah46@gmail.com>
2019-08-28 16:04:03 -0700

    added methods: 'add_trip', 'average_rating', 'total_revenue' to Driver class

--- lib/driver.rb
+++ lib/driver.rb
@@ -16,7 +16,40 @@ module RideShare
       @trips = trips || []
       
     end
+
+    def add_trip(trip)
+      # puts "#{trip.class.name} at trip"
+      @trips << trip
+    end
+
+    def average_rating
+      if @trips.empty?
+        return 0
+      end
+
+      ratings = @trips.map do |trip|
+        trip.rating.to_f
+      end
+      avg_rating = ratings.sum / ratings.length
+      return avg_rating
+    end
     
+    def total_revenue
+      if @trips == []
+        return 0
+      else
+        total = 0
+        @trips.each do |trip|
+          # puts trip.class.name
+          if trip.cost < 1.65
+          else
+            total += ((trip.cost - 1.65) * 0.8)
+          end
+        end
+        return total
+      end
+    end
+
     private
     
     def self.from_csv(record)
@@ -24,7 +57,7 @@ module RideShare
         id: record[:id],
         name: record[:name],
         vin: record[:vin],
-        status: record[:status]
+        status: record[:status].to_sym
       )
     end 
   end
