1b73b78d653da37668d1a7fff1c3408b0db1940a
Vi <vanessareigosa@gmail.com>
2019-08-28 15:18:35 -0700

    created driver class, succesfully passed in .load_all method

---
+++ lib/driver.rb
@@ -0,0 +1,38 @@
+require_relative 'csv_record'
+
+module RideShare
+  class Driver < CsvRecord 
+
+    attr_reader :name, :vin, :status, :trips
+
+    def initialize(id:, name:, vin:, status: AVAILABLE, trips: nil)
+      super(id)
+      @name = name
+      @status = status
+      @trips = trips || []
+
+      if vin.length == 17
+        @vin = vin 
+      else
+        raise ArgumentError
+      end
+    end
+
+    # Since `Driver` inherits from `CsvRecord`, you'll need to implement the `from_csv` template method. Once you do, `Driver.load_all` should work (test this in pry).
+    private 
+    
+    def self.from_csv(record)
+      return new(
+        id: record[:id],
+        name: record[:name],
+        vin: record[:vin],
+        status: record[:status],
+        trips: record[:trips]
+      )
+    end
+  end
+end
+
+
+
+
--- lib/trip.rb
+++ lib/trip.rb
@@ -4,11 +4,11 @@ require_relative 'csv_record'
 
 module RideShare
   class Trip < CsvRecord
-    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating
+    attr_reader :id, :passenger, :passenger_id, :start_time, :end_time, :cost, :rating, :driver, :driver_id
 
     def initialize(id:,
       passenger: nil, passenger_id: nil,
-      start_time:, end_time:, cost: nil, rating:)
+      start_time:, end_time:, cost: nil, rating:, driver: nil, driver_id: nil)
       super(id)
 
       if passenger
@@ -20,6 +20,17 @@ module RideShare
 
       else
         raise ArgumentError, 'Passenger or passenger_id is required'
+      end
+
+      if driver
+        @driver = driver
+        @driver_id = driver.id
+
+      elsif driver_id
+        @driver_id = driver_id
+
+      else
+        raise ArgumentError, 'Driver or driver_id is required'
       end
 
       #we want to make sure that start time is BEFORE end time
@@ -72,7 +83,8 @@ module RideShare
         start_time: start_time,
         end_time: end_time,
         cost: record[:cost],
-        rating: record[:rating]
+        rating: record[:rating],
+        driver_id: record[:driver_id]
         )       
 
     end
