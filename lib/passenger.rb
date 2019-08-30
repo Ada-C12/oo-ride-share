12b39fd8515bd01a43f136aacb789d45160682b6
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-30 12:20:30 -0700

    Defined method ‘ignore_incomplete_trips’ and implemented it in ‘total_time_spent’ and ‘net_expenditures’

--- lib/passenger.rb
+++ lib/passenger.rb
@@ -16,18 +16,33 @@ module RideShare
       @trips << trip
     end
     
+    def ignore_incomplete_trips(trips)
+      if trips.empty?
+        return []
+      end
+      copy_of_trips = trips.dup
+      copy_of_trips.delete_if do |trip|
+        trip.end_time == nil
+      end
+      return copy_of_trips
+    end
+    
     def net_expenditures
       total_spending = 0
-      @trips.each do |trip|
-        total_spending += trip.cost
+      unless @trips.empty?
+        ignore_incomplete_trips(@trips).each do |trip|
+          total_spending += trip.cost
+        end
       end
       return total_spending
     end
     
     def total_time_spent
       total_time = 0
-      @trips.each do |trip|
-        total_time += trip.duration
+      unless trips.empty?
+        ignore_incomplete_trips(@trips).each do |trip|
+          total_time += trip.duration
+        end
       end
       return total_time
     end
@@ -36,9 +51,9 @@ module RideShare
     
     def self.from_csv(record)
       return new(
-        id: record[:id],
-        name: record[:name],
-        phone_number: record[:phone_num]
+      id: record[:id],
+      name: record[:name],
+      phone_number: record[:phone_num]
       )
     end
   end
