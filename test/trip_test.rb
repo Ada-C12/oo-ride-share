2aaa77139822021b69823a3f3845a3fdd9c48276
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-30 12:28:05 -0700

    Updated test trip data to include driver data

--- test/trip_test.rb
+++ test/trip_test.rb
@@ -7,17 +7,22 @@ describe "Trip class" do
       start_time = Time.parse('2015-05-20T12:14:00+00:00')
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
-        id: 8,
-        passenger: RideShare::Passenger.new(id: 1,
-          name: "Ada",
-          phone_number: "412-432-7640"
-        ),
-        start_time: start_time,
-        end_time: end_time,
-        cost: 23.45,
-        rating: 3,
-        driver_id: 3
-      }
+      id: 8,
+      passenger: RideShare::Passenger.new(id: 1,
+      name: "Ada",
+      phone_number: "412-432-7640"
+      ),
+      start_time: start_time,
+      end_time: end_time,
+      cost: 23.45,
+      rating: 3,
+      driver: RideShare::Driver.new(id: 3,
+      name: "Katherine",
+      vin: "MP97K0AY5U7G2CHPM",
+      status: :AVAILABLE,
+      trips: nil
+      ) }
+      
       @trip = RideShare::Trip.new(@trip_data)
     end
     
@@ -30,7 +35,6 @@ describe "Trip class" do
     end
     
     it "stores an instance of driver" do
-      skip # Unskip after Wave 2
       expect(@trip.driver).must_be_kind_of RideShare::Driver
     end
     
@@ -56,30 +60,30 @@ describe "Trip class" do
       start_time = Time.parse('2015-05-20T12:14:00+00:00')
       end_time = start_time + 25 * 60 # 25 minutes
       @trip_data = {
-        id: 8,
-        passenger: RideShare::Passenger.new(id: 1,
-          name: "Ada",
-          phone_number: "412-432-7640"
-        ),
-        start_time: start_time,
-        end_time: end_time,
-        cost: 23.45,
-        rating: 3,
-        driver_id: 3
-      }
-    end
-    
-    it "calculates the total trip time" do
-      trip = RideShare::Trip.new(@trip_data)
-      result = trip.duration
-      expect(result).must_equal 1500
-    end
-    
-    it "returns 0 if start and end time were the same" do
-      @trip_data[:end_time] = @trip_data[:start_time]
-      trip = RideShare::Trip.new(@trip_data)
-      result = trip.duration
-      expect(result).must_equal 0
-    end
+      id: 8,
+      passenger: RideShare::Passenger.new(id: 1,
+      name: "Ada",
+      phone_number: "412-432-7640"
+      ),
+      start_time: start_time,
+      end_time: end_time,
+      cost: 23.45,
+      rating: 3,
+      driver_id: 3
+    }
   end
+  
+  it "calculates the total trip time" do
+    trip = RideShare::Trip.new(@trip_data)
+    result = trip.duration
+    expect(result).must_equal 1500
+  end
+  
+  it "returns 0 if start and end time were the same" do
+    @trip_data[:end_time] = @trip_data[:start_time]
+    trip = RideShare::Trip.new(@trip_data)
+    result = trip.duration
+    expect(result).must_equal 0
+  end
+end
 end
