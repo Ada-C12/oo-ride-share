d5a3d59793067bda21c2844d32fe2e756d360e2e
Michaela Morrow <morrow.michaela@gmail.com>
2019-08-30 12:17:02 -0700

    Checked validity of driver status, defined helper method ‘add_in_progress_trip’, made driver methods ignore in-progress trips

--- lib/driver.rb
+++ lib/driver.rb
@@ -2,7 +2,8 @@ require_relative 'csv_record'
 
 module RideShare
   class Driver < CsvRecord
-    attr_reader :name, :vin, :status, :trips
+    attr_reader :name, :vin, :trips
+    attr_accessor :status
     
     def initialize(id:, name:, vin:, status: :AVAILABLE, trips: nil)
       super (id)
@@ -10,24 +11,45 @@ module RideShare
         raise ArgumentError.new "Error! Vin must contain exactly 17 characters."
       end
       
+      valid_statuses = [:AVAILABLE, :UNAVAILABLE]
+      if !valid_statuses.include?(status.to_sym)
+        raise ArgumentError.new "Error! The status you entered is invalid."
+      end
+      
       @name = name
       @vin = vin
-      @status = status
+      @status = status.to_sym
       @trips = trips || []
       
     end
-
+    
     def add_trip(trip)
-      # puts "#{trip.class.name} at trip"
       @trips << trip
     end
-
+    
+    def add_in_progress_trip(trip)
+      trip.driver.status = :UNAVAILABLE
+      @trips << trip
+    end
+    
+    def ignore_incomplete_trips(trips)
+      if trips.empty?
+        return []
+      end
+      copy_of_trips = trips.dup
+      copy_of_trips.delete_if do |trip|
+        trip.end_time == nil
+      end
+      return copy_of_trips
+    end
+    
     def average_rating
-      if @trips.empty?
+      valid_trips = ignore_incomplete_trips(@trips)
+      if valid_trips.empty?
         return 0
       end
-
-      ratings = @trips.map do |trip|
+      
+      ratings = valid_trips.map do |trip|
         trip.rating.to_f
       end
       avg_rating = ratings.sum / ratings.length
@@ -35,12 +57,12 @@ module RideShare
     end
     
     def total_revenue
-      if @trips == []
+      valid_trips = ignore_incomplete_trips(@trips)
+      if valid_trips == []
         return 0
       else
         total = 0
-        @trips.each do |trip|
-          # puts trip.class.name
+        valid_trips.each do |trip|
           if trip.cost < 1.65
           else
             total += ((trip.cost - 1.65) * 0.8)
@@ -49,15 +71,15 @@ module RideShare
         return total
       end
     end
-
+    
     private
     
     def self.from_csv(record)
       return new(
-        id: record[:id],
-        name: record[:name],
-        vin: record[:vin],
-        status: record[:status].to_sym
+      id: record[:id],
+      name: record[:name],
+      vin: record[:vin],
+      status: record[:status].to_sym
       )
     end 
   end
